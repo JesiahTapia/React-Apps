67a029f0a5a0e90114b7a6fcb1b277fb736a46f7
JesiahTapia <siahtapia@gmail.com>
2024-05-25 20:30:19 -0400

    updated react app

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+// use PacalCasing not camelCasing
+function Message () {
+// this is JSX : JavaScript XML this changes the TypeScript to JavaScript
+const name = 'Jesiah'
+if (name)  //truthy statement
+return <h1>Hello {name}</h1>
+return <h1>Hello World</h1>
+}
+
+export default Message
