34e60dbab71208f902c295b06310276a1e6d7d3f
JesiahTapia <siahtapia@gmail.com>
2024-05-26 20:15:57 -0400

    React App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 // import Message from './Message'
 
-import ListGroup from "./components/ListGroup"
-function App () {
-return <div><ListGroup></ListGroup></div>
+import ListGroup from "./components/ListGroup";
+function App() {
+  const items = ["Tokyo", "Hawaii", "Greece", "Egypt", "Korea"];
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
